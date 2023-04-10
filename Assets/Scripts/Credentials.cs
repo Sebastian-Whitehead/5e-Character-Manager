11d34a16c282612551e1305bcc337547d0e2c337
Sebastian Whitehead <sebastianmikael2000@gmail.com>
2023-04-10 21:06:39 +0200

    Shifting To External Character Manager
    Rather than building an entire character builder the idea of using the existing data from the Aurora Character Builder was preferred. Hereby shifting to a character manager premiss instead

--- Assets/Scripts/Credentials.cs
+++ Assets/Scripts/Credentials.cs
@@ -1,7 +1,17 @@
+/*File for storing user specific information that should not be pushed to github
+> git update-index --assume-unchanged Credentials.cs 
+> git update-index --no-assume-unchanged Credentials.cs         // Undo the undo credentials github ignore.
+*/
+
 namespace Credentials
 {
-    public static class Credentials
+    public class Credentials
     {
         public const string ApiKey = "Your API Key";
+    }
+
+    public class FileLocation
+    {
+        
     }
 }
